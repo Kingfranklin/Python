da653f3d7f79c4f5ddcf1d773971f6d94ea4ea4b
Franklin Nweke <franklinnweke@Franklins-Air.lan>
2023-01-22 15:14:02 +0000

    day1 Practise

---
+++ day1/day1_pra.py
@@ -0,0 +1,18 @@
+var = 2
+print(var)
+
+# user input example 
+
+name = input("What is yout name? ")
+age = int(input("How old are you? "))
+print(f"your name is {name} and you are {age} old")
+
+
+# user input example
+
+height = input ("how short is promise? ")
+midget = input ("is she a midget? ")
+print (f"promise height is {height} and is she a midget {midget} ")
+
+# user input example
+hour = input ("")
