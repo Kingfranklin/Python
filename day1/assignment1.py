91e778d89524d32b290bc160f59a4f8a5fbe0562
Franklin Nweke <franklinnweke@Franklins-Air.lan>
2023-02-12 17:24:18 +0000

    assignment

---
+++ day1/assignment1.py
@@ -0,0 +1,18 @@
+name = input("what is your name")
+print (f"Welcome to Jtech {name} ") 
+
+# write a programme that will accpet two numbers from the user and caculates multiplication
+
+number1 = int(input("enter first number"))
+number2= int(input("enter second number"))
+total = number1 * number2
+print (f"{number1} multipy {number2} is {total}") 
+
+# write a programme that would ask student for thier names 
+
+student1 = input("what is your name")
+student2 = input("what is your name")
+student3 = input("what is your name")
+print (f"welcome to Jtech {student1}  {student2} {student3}")
+
+
