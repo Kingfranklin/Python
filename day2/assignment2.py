87c92acd86bf73b8edeb1b805f3fadfbba2f046c
Franklin Nweke <franklinnweke@Franklins-Air.lan>
2023-02-14 18:46:09 +0000

    add files to github

---
+++ day2/assignment2.py
@@ -0,0 +1,5 @@
+print ("Welcome to Jtech")
+city = input("what city did you grow up? \n")
+pet = input("what is the name of your pet? \n")
+bandname = city + pet 
+print (f"Thanks for using Jtech. your bandname is {bandname}")
