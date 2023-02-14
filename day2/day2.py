8a49bf117c65f6e5c58e286e963c746f0d9598dc
Franklin Nweke <franklinnweke@Franklins-Air.lan>
2023-02-14 19:17:49 +0000

    add files to day2

--- day2/day2.py
+++ day2/day2.py
@@ -9,3 +9,13 @@ if x > 80 :
     print ("bigger")
 
 print("finish")
+
+#Write a python script that prompts the user for their age, and if they are over 18,
+#prints a message saying they are eligible to vote, and if they are under 18, 
+#prints a message saying they are not eligible to vote
+
+age = int(input("how old are you?\n"))
+if age >= 18:
+    print ("eligible to vote")
+else: 
+    print ("not eligible to vote")    
