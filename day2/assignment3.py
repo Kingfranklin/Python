84cc4c5e3be562e9b81da6ead5f469b953fe2ba3
Franklin Nweke <franklinnweke@Franklins-Air.lan>
2023-02-20 13:41:23 +0000

    asssigment3

---
+++ day2/assignment3.py
@@ -0,0 +1,9 @@
+print ("Hello World")
+
+#3 write a program to check if the number entered by a user is even or odd
+
+number= int(input ("enter a number\n"))
+if number%2==0:
+    print ("even number")
+else:
+    print ("odd number")
